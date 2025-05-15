9f75e144864c429a7cd7c47ffbf3f4b88e930919
gaimerI17 <gaimeri255@gmail.com>
2025-05-15 08:21:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,98 @@
 let deviceMotionAccelX;
+let deviceMotionAccelY;
+let deviceMotionAccelZ;
+let deviceRotationRateA;
+let deviceRotationRateB;
+let deviceRotationRateC;
+let deviceMotionInterval;
+
 window.addEventListener("devicemotion", (event) => {
   deviceMotionAccelX = event.acceleration.x;
+  deviceMotionAccelY = event.acceleration.y;
+  deviceMotionAccelZ = event.acceleration.z;
+  deviceRotationRateA = event.rotationRate.alpha;
+  deviceRotationRateB = event.rotationRate.beta;
+  deviceRotationRateC = event.rotationRate.gamma;
+  deviceMotionInterval = event.interval;
 });
 
 class gaimeriWebAPIExtension {
   getInfo() {
     return {
-      id: 'gaimeriwebapiextensio 
-n',
+      id: 'gaimeriwebapiextension',
       name: 'Web APIs',
       blocks: [
         {
           opcode: 'deviceAccelerationX',
-          blockType: 
-
-ratch.BlockType.REPORTER,
+          blockType: Scratch.BlockType.REPORTER,
           text: 'device acceleration x',
           disableMonitor: false
-        }
+        },
+        {
+          opcode: 'deviceAccelerationY',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'device acceleration y',
+          disableMonitor: false
+        },
+        {
+          opcode: 'deviceAccelerationZ',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'device acceleration z',
+          disableMonitor: false
+        },
+        {
+          opcode: 'deviceRotationA',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'device rotation rate alpha',
+          disableMonitor: false
+        },
+        {
+          opcode: 'deviceRotationB',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'device rotation rate beta',
+          disableMonitor: false
+        },
+        {
+          opcode: 'deviceRotationC',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'device rotation rate gamma',
+          disableMonitor: false
+        },
+        {
+          opcode: 'deviceMotionInterval',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'device motion capture interval',
+          disableMonitor: false
+        },
       ]
     };
   }
   deviceAccelerationX(){
     return deviceMotionAccelX;
+  }
+
+  deviceAccelerationY(){
+    return deviceMotionAccelY;
+  }
+
+  deviceAccelerationZ(){
+    return deviceMotionAccelZ;
+  }
+
+  deviceRotationA(){
+    return deviceRotationRateA;
+  }
+
+  deviceRotationB(){
+    return deviceRotationRateB;
+  }
+
+  deviceRotationC(){
+    return deviceRotationRateC;
+  }
+
+  deviceMotionInterval(){
+    return deviceMotionInterval;
   }
 }
 
